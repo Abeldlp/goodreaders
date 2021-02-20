76042463e389b8d3790c32c6cd5d20a14a98e3a8
Abel de la Paz <abel@foodticket.nl>
2021-02-20 22:21:35 +0100

    Added crud functionality for Book

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,83 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SaveBookRequest;
+use App\Models\Book;
+use App\Models\Rating;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        return Book::with('user')->with('score')->get();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function store(SaveBookRequest $request)
     {
-        //
+        $validatedData =  $this->validate($request, $request->messages());
+
+        (new Book())->create($validatedData);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        //
+        return [
+            'book' => Book::find($id),
+            'ratings' => Rating::where('book_id', $id)->with('user')->with('replies')->get()
+        ];
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    public function update(SaveBookRequest $request, $id)
     {
-        //
+        $validatedData = $this->validate($request, $request->messages());
+
+        (new Book())::find($id)->update($validatedData);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+        Book::destroy($id);
     }
 }
