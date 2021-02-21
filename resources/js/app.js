6bfb70109a2cd19547cbf86e975a1acf94837216
Abel de la Paz <abel@foodticket.nl>
2021-02-21 13:30:08 +0100

    Setup frontend spa with vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,32 +1,54 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
 
-window.Vue = require('vue').default;
+// window.Vue = require('vue').default;
+//
+// /**
+//  * The following block of code may be used to automatically register your
+//  * Vue components. It will recursively scan this directory for the Vue
+//  * components and automatically register them with their "basename".
+//  *
+//  * Eg. ./components/App.vue -> <example-component></example-component>
+//  */
+//
+// //const app = document.getElementById('app')
+//
+// // const files = require.context('./', true, /\.vue$/i)
+// // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+//
+// Vue.component('app', require('./components/App.vue').default);
+//
+// const app = new Vue({
+//     el: '#app',
+// });
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
 
-const files = require.context('./', true, /\.vue$/i)
-files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.use(VueRouter)
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+import App from './components/App'
+import Main from './components/views/Index'
+import Test from './components/views/Test'
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'main',
+            component: Main
+        },
+        {
+            path: '/test',
+            name: 'test',
+            component: Test,
+        },
+    ],
+});
 
 const app = new Vue({
     el: '#app',
+    components: { App },
+    router,
 });
---
+++ resources/js/components/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1 class="ml-3">Nav of the app goes here</h1>
+        <p class="ml-5">
+            <router-link :to="{ name: 'main' }">Main</router-link> |
+            <router-link :to="{ name: 'test' }">Test</router-link> |
+            <a href="/login">Login</a> |
+            <a href="/register">Register</a>
+        </p>
+
+        <div class="container">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'App',
+    }
+</script>
