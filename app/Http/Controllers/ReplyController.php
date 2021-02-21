0a538be6c8162c6bbb01725057741831a6391ccc
Abel de la Paz <abel@foodticket.nl>
2021-02-21 12:08:13 +0100

    Added create, update and delete functionality to replies

--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -2,43 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Rating;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ReplyController extends Controller
 {
 
-    public function index()
-    {
-        //
-    }
-
-    public function create()
-    {
-        //
-    }
-
     public function store(Request $request)
     {
-        //
-    }
+        $validatedData = $request->validate([
+            'rating_id' => 'integer|required',
+            'user_id' => '',
+            'reply' => 'string|required'
+        ]);
 
-    public function show($id)
-    {
-        //
-    }
+        $withUserId = array_merge($validatedData, ['user_id' => Auth::id()]);
 
-    public function edit($id)
-    {
-        //
+        (new Rating())->create($withUserId);
     }
 
     public function update(Request $request, $id)
     {
-        //
+        $validatedData = $request->validate([
+            'rating_id' => 'integer|required',
+            'user_id' => '',
+            'reply' => 'string|required'
+        ]);
+
+        $withUserId = array_merge($validatedData, ['user_id' => Auth::id()]);
+
+        (new Rating())->find($id)->update($withUserId);
     }
 
     public function destroy($id)
     {
-        //
+        (new Rating())->destroy($id);
     }
 }
